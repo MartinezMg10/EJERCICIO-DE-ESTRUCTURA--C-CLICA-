1b7efdfe6346ccfe4e1a65b9631fe08159095de4
Oalvarez <oalvarez500@gmail.com>
2022-12-13 10:21:45 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+/* 1. Sumar una cantidad N números MAYORES a 0 utilizando MIENTRAS. */
+
+
+/* 
+2. Imprimir números aleatorios en el rango de 1 a 10 mientras el número no sea igual a cero. */
+
+
+
+/* 
+3. Imprimir 20 números aleatorios en el rango de 1 a 1000. */
+
+
+
+
+/* 4. Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla
+todos los números impares desde 1 hasta ese número. */
+
+
+
+/* 5. Una persona debe realizar un muestreo con 20 personas para determinar el promedio de peso
+de los niños, jóvenes, adultos y viejos que existen en su zona habitacional. Se determinan las
+categorías con base en la sig., tabla:
+CATEGORIA EDAD
+Niños 0 - 12
+Jóvenes 13 - 29
+Adultos 30 - 59
+Viejos 60 en adelante */
+
+
+
+/* 6. Diseñen un algoritmo que imprima la siguiente secuencia:
+1.1.1 - 1.1.2 - 1.1.3 - 1.1.4
+1.2.1 - 1.2.2 - 1.2.3 - 1.2.4
+1.3.1 - 1.3.2 - 1.3.3 - 1.3.4
+1.4.1 - 1.4.2 - 1.4.3 - 1.4.4
+1.5.1 - 1.5.2 - 1.5.3 - 1.5.4 */
+
+
+
+
+/* 7. Un Zoólogo pretende determinar el porcentaje de animales que hay en las siguientes tres
+categorías de edades: de 0 a 1 año, de más de 1 año y menos de 3 y de 3 o más años. El
+zoológico todavía no está seguro del animal que va a estudiar. Si se decide por elefantes solo
+tomara una muestra de 20 de ellos; si se decide por las jirafas, tomara 15 muestras, y si son
+chimpancés tomara 40. */
+
+
+/* 
+8. Una compañía de seguros tiene contratados a una cantidad N vendedores. Cada vendedor
+hace tres ventas a la semana. Su política de pagos es que un vendedor recibe un sueldo base, y
+además un 10% extra por comisiones del total de sus ventas. El gerente de la compañía desea
+saber cuánto dinero recibirá cada vendedor en una semana por concepto de comisiones por
+las ventas, y cuanto será el total tomando en cuenta su sueldo base y sus comisiones. */
+
+
+/* 
+9. Una agencia de venta de autos paga a su personal de ventas un salario de $950.000, más una
+comisión de $170.000 por cada auto vendido, y también un 5% extra del valor total de las
+ventas. Diseñar un algoritmo para calcular el salario de un vendedor en un determinado mes,
+realizando la lectura por pantalla del nº de automóviles vendidos y el valor de cada auto para
+hallar el monto total de ventas. */
+
+
+
+/* 
+10. Hallar el promedio de calificaciones de un estudiante, teniendo en cuenta el nombre estudiante
+y 5 notas; calculando la nota final de acuerdo a los siguientes porcentajes: Dos (2) notas valen
+el 40% y las otras tres (3) valen el 60%.  */
